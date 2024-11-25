bf18362b9dcf0834bb2418076d98b103345ef27d
Huy Nguyen <nguyenmaibaohuy2003@gmail.com>
2024-11-25 11:35:54 +0700

    feat: init provide service

---
+++ provide-service/src/main/java/olp/hutechfoss/provideservice/ProvideServiceApplication.java
@@ -0,0 +1,13 @@
+package olp.hutechfoss.provideservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvideServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvideServiceApplication.class, args);
+    }
+
+}
---
+++ provide-service/src/test/java/olp/hutechfoss/provideservice/ProvideServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package olp.hutechfoss.provideservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvideServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
