7f850f96328fc3fd20f27aa6bb59b778577fa979
PoTLap <quanganhzzz8@gmail.com>
2024-12-04 17:03:50 +0700

    done

--- project/settings.py
+++ project/settings.py
@@ -1,6 +1,6 @@
 import os
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "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")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
 
 DB_HOST = "localhost"
 DB_USER = "root"
