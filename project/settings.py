5971a16f2fec9b264c39e40b8e676372c187e25b
PoTLap <quanganhzzz8@gmail.com>
2024-12-04 17:01:43 +0700

    done

---
+++ project/settings.py
@@ -0,0 +1,12 @@
+import os
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "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")
+
+DB_HOST = "localhost"
+DB_USER = "root"
+DB_PASSWORD = ""
+DB_NAME = "supply_chain_future"
+
+LOG_DIR = "./logs"
+APP_NAME = "supporter"
+LOGGING_ENABLED = True
