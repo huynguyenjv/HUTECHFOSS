ee12fe09215a0f8d711c81a76312d813d0d88e9d
PoTLap <quanganhzzz8@gmail.com>
2024-12-04 17:13:32 +0700

    Remove all files from staging

--- project/utils/response_formatter.py
+++
@@ -1,7 +0,0 @@
-def format_response(data):
-    if not data:
-        return "Không tìm thấy dữ liệu."
-    response = "Kết quả:\n"
-    for row in data:
-        response += ", ".join([f"{key}: {value}" for key, value in row.items()]) + "\n"
-    return response
