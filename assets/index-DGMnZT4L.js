763574482b09ccaf0f01e6963dea6d23a2466f20
Huy Nguyen <nguyenmaibaohuy2003@gmail.com>
2024-12-08 16:54:46 +0700

    Updates

---
+++ assets/index-DGMnZT4L.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-B4A1g6Ej.js";function r(){return e.jsx("div",{children:"Home"})}export{r as default};
--- src/features/HelperForm/index.jsx
+++
@@ -1,9 +0,0 @@
-import ReliefForm from "./components/ReliefForm";
-
-export default function HelperForm() {
-    return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
-            <ReliefForm />
-        </div>
-    );
-}
