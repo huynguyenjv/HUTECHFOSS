10fd5a7f8c671de1416f034d6dd477234f648844
Huy Nguyen <nguyenmaibaohuy2003@gmail.com>
2024-12-12 02:14:57 +0700

    fix: remove file

--- admin/src/main/webapp/app/entities/reducers.ts
+++ admin/src/main/webapp/app/entities/reducers.ts
@@ -1,28 +1,6 @@
-import chiTietDieuPhoi from 'app/entities/chi-tiet-dieu-phoi/chi-tiet-dieu-phoi.reducer';
-import loaiNguoiNhan from 'app/entities/loai-nguoi-nhan/loai-nguoi-nhan.reducer';
-import loaiNhuYeuPham from 'app/entities/loai-nhu-yeu-pham/loai-nhu-yeu-pham.reducer';
-import loaiThienTai from 'app/entities/loai-thien-tai/loai-thien-tai.reducer';
-import nguoiNhan from 'app/entities/nguoi-nhan/nguoi-nhan.reducer';
-import nhaCungCap from 'app/entities/nha-cung-cap/nha-cung-cap.reducer';
-import nhuYeuPham from 'app/entities/nhu-yeu-pham/nhu-yeu-pham.reducer';
-import phieuDieuPhoi from 'app/entities/phieu-dieu-phoi/phieu-dieu-phoi.reducer';
-import tinhNguyenVien from 'app/entities/tinh-nguyen-vien/tinh-nguyen-vien.reducer';
-import trangThai from 'app/entities/trang-thai/trang-thai.reducer';
-import vung from 'app/entities/vung/vung.reducer';
 /* jhipster-needle-add-reducer-import - JHipster will add reducer here */
 
 const entitiesReducers = {
-  chiTietDieuPhoi,
-  loaiNguoiNhan,
-  loaiNhuYeuPham,
-  loaiThienTai,
-  nguoiNhan,
-  nhaCungCap,
-  nhuYeuPham,
-  phieuDieuPhoi,
-  tinhNguyenVien,
-  trangThai,
-  vung,
   /* jhipster-needle-add-reducer-combine - JHipster will add reducer here */
 };
 
--- admin/src/main/webapp/app/entities/routes.tsx
+++ admin/src/main/webapp/app/entities/routes.tsx
@@ -2,17 +2,6 @@ import React from 'react';
 
 import ErrorBoundaryRoutes from 'app/shared/error/error-boundary-routes';
 
-import ChiTietDieuPhoi from './chi-tiet-dieu-phoi';
-import LoaiNguoiNhan from './loai-nguoi-nhan';
-import LoaiNhuYeuPham from './loai-nhu-yeu-pham';
-import LoaiThienTai from './loai-thien-tai';
-import NguoiNhan from './nguoi-nhan';
-import NhaCungCap from './nha-cung-cap';
-import NhuYeuPham from './nhu-yeu-pham';
-import PhieuDieuPhoi from './phieu-dieu-phoi';
-import TinhNguyenVien from './tinh-nguyen-vien';
-import TrangThai from './trang-thai';
-import Vung from './vung';
 /* jhipster-needle-add-route-import - JHipster will add routes here */
 
 export default () => {
@@ -20,17 +9,6 @@ export default () => {
     <div>
       <ErrorBoundaryRoutes>
         {/* prettier-ignore */}
-        <Route path="chi-tiet-dieu-phoi/*" element={<ChiTietDieuPhoi />} />
-        <Route path="loai-nguoi-nhan/*" element={<LoaiNguoiNhan />} />
-        <Route path="loai-nhu-yeu-pham/*" element={<LoaiNhuYeuPham />} />
-        <Route path="loai-thien-tai/*" element={<LoaiThienTai />} />
-        <Route path="nguoi-nhan/*" element={<NguoiNhan />} />
-        <Route path="nha-cung-cap/*" element={<NhaCungCap />} />
-        <Route path="nhu-yeu-pham/*" element={<NhuYeuPham />} />
-        <Route path="phieu-dieu-phoi/*" element={<PhieuDieuPhoi />} />
-        <Route path="tinh-nguyen-vien/*" element={<TinhNguyenVien />} />
-        <Route path="trang-thai/*" element={<TrangThai />} />
-        <Route path="vung/*" element={<Vung />} />
         {/* jhipster-needle-add-route-path - JHipster will add routes here */}
       </ErrorBoundaryRoutes>
     </div>
